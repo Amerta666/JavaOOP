ea805d2cb3a39c4002d569713bcaa0e94485af92
Amerta666 <beponuka666@gmail.com>
2023-08-10 22:45:02 +0500

    Add Final HomeWork 7

--- HomeWork/hm7/Main.java
+++ HomeWork/hm7/Main.java
@@ -4,6 +4,6 @@ public class Main {
         CalcModel model = new CalcModel();
         CalcView view = new CalcView();
         CalcPresenter presenter = new CalcPresenter(model, view);
-        presenter.calculat();
+        presenter.calculate();
     }
 }
